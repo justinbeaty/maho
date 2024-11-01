79be7b30a0bf8f43f9433b8312b671bc3314570d
Justin Beaty <justin.beaty@gmail.com>
2024-11-01 16:10:00 -0700

    errors

--- app/Mage.php
+++ app/Mage.php
@@ -14,6 +14,8 @@ define('DS', DIRECTORY_SEPARATOR);
 define('PS', PATH_SEPARATOR);
 define('BP', MAHO_ROOT_DIR);
 
+foobar
+
 /*
  * Require Composer autoloader and set include paths
  * @var \Composer\Autoload\ClassLoader $composerClassLoader
