802024f48308d047fb6880f074d836e640b09377
Justin Beaty <justin.beaty@gmail.com>
2024-12-13 11:07:10 -0800

    Fix promo page

--- app/code/core/Mage/Adminhtml/Block/Catalog/Category/Checkboxes/Tree.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Category/Checkboxes/Tree.php
@@ -20,9 +20,6 @@ class Mage_Adminhtml_Block_Catalog_Category_Checkboxes_Tree extends Mage_Adminht
 {
     protected $_selectedIds = [];
 
-    /**
-     * @return $this
-     */
     #[\Override]
     protected function _prepareLayout()
     {
@@ -30,11 +27,18 @@ class Mage_Adminhtml_Block_Catalog_Category_Checkboxes_Tree extends Mage_Adminht
         return $this;
     }
 
+
     public function getCategoryIds()
     {
         return $this->_selectedIds;
     }
 
+    /**
+     *
+     *
+     * @param array $ids
+     * @return $this
+     */
     public function setCategoryIds($ids)
     {
         if (empty($ids)) {
@@ -46,33 +50,24 @@ class Mage_Adminhtml_Block_Catalog_Category_Checkboxes_Tree extends Mage_Adminht
         return $this;
     }
 
+    /**
+     * Returns root node and sets 'checked' flag (if necessary)
+     */
+    public function getRootNode(): Varien_Data_Tree_Node
+    {
+        $root = $this->getRoot();
+        if ($root && in_array($root->getId(), $this->getCategoryIds())) {
+            $root->setChecked(true);
+        }
+        return $root;
+    }
+
     #[\Override]
     protected function _getNodeJson($node, $level = 1)
     {
-        $item = [];
-        $item['text'] = $this->escapeHtml($node->getName());
+        $item = parent::_getNodeJson($node, $level);
 
-        if ($this->_withProductCount) {
-            $item['text'] .= ' (' . $node->getProductCount() . ')';
-        }
-        $item['id']  = $node->getId();
-        $item['path'] = $node->getData('path');
-        $item['cls'] = 'folder ' . ($node->getIsActive() ? 'active-category' : 'no-active-category');
-        $item['allowDrop'] = false;
-        $item['allowDrag'] = false;
-
-        if ($node->hasChildren()) {
-            $item['children'] = [];
-            foreach ($node->getChildren() as $child) {
-                $item['children'][] = $this->_getNodeJson($child, $level + 1);
-            }
-        }
-
-        if (empty($item['children']) && (int)$node->getChildrenCount() > 0) {
-            $item['children'] = [];
-        }
-
-        if (!empty($item['children'])) {
+        if ($this->_isParentSelectedCategory($node)) {
             $item['expanded'] = true;
         }
 
@@ -81,11 +76,5 @@ class Mage_Adminhtml_Block_Catalog_Category_Checkboxes_Tree extends Mage_Adminht
         }
 
         return $item;
-    }
-
-    #[\Override]
-    public function getRoot($parentNodeCategory = null, $recursionLevel = 3)
-    {
-        return $this->getRootByIds($this->getCategoryIds());
     }
 }
