5048b3b972fc1ebc9c45f1f21cf4a754ccdec518
Justin Beaty <justin.beaty@gmail.com>
2025-01-17 08:10:44 -0800

    dashboard

--- public/js/mage/adminhtml/dashboard.js
+++ public/js/mage/adminhtml/dashboard.js
@@ -4,27 +4,100 @@ class MahoDashboard
 
     constructor(config = {}) {
         this.config = config;
+
+        console.log(config)
+
+        document.addEventListener('DOMContentLoaded', this.bindEventListeners.bind(this));
     }
 
-    async changeDiagramsPeriod(periodObj) {
-        periodParam = periodObj.value ? 'period/' + periodObj.value + '/' : '';
+    bindEventListeners() {
+        document.getElementById(this.config.diagrams?.switcherId)?.addEventListener('change', (event) => {
+            this.changeDiagramsPeriod(event.target);
+        });
+    }
 
-        for (const tabId of this.config.tabIds) {
-            const html = await mahoFetch(setRouteParams(this.config.ajaxUrl, {
+    async changeDiagramsPeriod(periodSwitcherEl) {
+        if (!Array.isArray(this.config.diagrams?.tabs)) {
+            return;
+        }
+
+        for (const tabId of this.config.diagrams.tabs) {
+            const html = await mahoFetch(setRouteParams(this.config.diagrams.ajaxUrl, {
                 block: `tab_${tabId}`,
-                period: periodObj.value,
+                period: periodSwitcherEl.value,
             }));
 
-            const tabContentEl = document.getElementById(`${this.id}_${tabId}_content`);
+            const tabContentEl = document.getElementById(`${this.config.diagrams.htmlId}_${tabId}_content`);
             updateElementHtmlAndExecuteScripts(tabContentEl, html);
         }
 
-        const html = await mahoFetch(setRouteParams(this.config.ajaxUrl, {
+        const html = await mahoFetch(setRouteParams(this.config.diagrams.ajaxUrl, {
             block: 'totals',
         }));
 
         const tabContentEl = document.getElementById('dashboard_diagram_totals');
         updateElementHtmlAndExecuteScripts(tabContentEl, html);
+    }
+
+    initializeChart(canvasId, datasets, labels) {
+        const canvas = document.getElementById(canvasId);
+        if (!canvas) {
+            return;
+        }
+
+        const borderColor = '#adb41a';
+        const backgroundColor = canvas.getContext('2d').createLinearGradient(0, 0, 0, 293);
+        backgroundColor.addColorStop(0, `${borderColor}aa`);
+        backgroundColor.addColorStop(1, `${borderColor}00`);
+
+
+        const config = {
+            type: 'line',
+            data: {
+                labels: labels.map((date) => {
+                    return date;
+                    const year = date.substr(3, 6);
+                    const month = date.substr(0, 2);
+                    return new Date(year, month - 1);
+                }),
+                datasets: Object.entries(datasets).map(([id, data]) => ({
+                    id,
+                    data,
+                    borderColor,
+                    fill: true,
+                    backgroundColor,
+                    //pointRadius: 0,
+                    tension: 0.2,
+                })),
+            },
+            options: {
+                scales: {
+                    x: {
+                        //type: 'time',
+                    },
+                    y: {
+                        suggestedMin: 0,
+                        suggestedMax: 10,
+                        ticks: {
+                            // Include a dollar sign in the ticks
+                            callback: function(value, index, ticks) {
+                                console.log(value, index, ticks)
+                                return '$' + value;
+                            }
+                        },
+                    },
+                },
+                animation: {
+                    duration: 0
+                },
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                }
+            }
+        };
+        new Chart(canvasId, config);
     }
 
 }
