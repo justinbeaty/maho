690026902d3eec250b27d4e8ea72f4f8c57ad0b1
Justin Beaty <justin.beaty@gmail.com>
2025-01-17 09:33:40 -0800

    dashboard

--- public/js/mage/adminhtml/dashboard.js
+++ public/js/mage/adminhtml/dashboard.js
@@ -7,6 +7,7 @@ class MahoDashboard
 
         console.log(config)
 
+        this.initializeChartJs();
         document.addEventListener('DOMContentLoaded', this.bindEventListeners.bind(this));
     }
 
@@ -39,19 +40,60 @@ class MahoDashboard
         updateElementHtmlAndExecuteScripts(tabContentEl, html);
     }
 
-    initializeChart(canvasId, datasets, labels) {
+    drawChart(canvasId, datasets, labels) {
         const canvas = document.getElementById(canvasId);
         if (!canvas) {
             return;
         }
 
+        const ctx = canvas.getContext('2d');
+
         const borderColor = '#adb41a';
-        const backgroundColor = canvas.getContext('2d').createLinearGradient(0, 0, 0, 293);
+        const backgroundColor = ctx.createLinearGradient(0, 0, 0, 293);
         backgroundColor.addColorStop(0, `${borderColor}aa`);
         backgroundColor.addColorStop(1, `${borderColor}00`);
 
+        const config = {
+            type: 'line',
+            data: {
+                labels,
+                datasets: Object.entries(datasets).map(([id, data]) => ({
+                    id,
+                    data,
+                    borderColor,
+                    fill: true,
+                    backgroundColor,
+                    pointRadius: 0,
+                    tension: 0.2,
+                })),
+            },
+            options: {
+                interaction: {
+                    intersect: false,
+                    mode: 'index',
+                    position: 'cursor',
+                },
+                scales: {
+                    y: {
+                        suggestedMin: 0,
+                        suggestedMax: 10,
+                    },
+                },
+            },
+        };
+        new Chart(ctx, config);
+    }
 
-        const plugin = {
+
+    initializeChartJs() {
+        Chart.defaults.animation.duration = 0;
+        Chart.defaults.plugins.tooltip.animation = false;
+        Chart.defaults.plugins.legend.display = false;
+
+        Chart.Tooltip.positioners.cursor = (_, coordinates) => coordinates;
+
+        Chart.register({
+            id: 'linePlugin',
             afterInit: (chart, args, opts) => {
                 chart.hoverState = { x: 0, draw: false };
             },
@@ -74,69 +116,7 @@ class MahoDashboard
                 ctx.stroke()
                 ctx.restore()
             }
-        }
-
-        Chart.Tooltip.positioners.cursor = (_, coordinates) => coordinates;
-
-        const config = {
-            type: 'line',
-            data: {
-                labels: labels.map((date) => {
-                    return date;
-                    const year = date.substr(3, 6);
-                    const month = date.substr(0, 2);
-                    return new Date(year, month - 1);
-                }),
-                datasets: Object.entries(datasets).map(([id, data]) => ({
-                    id,
-                    data,
-                    borderColor,
-                    fill: true,
-                    backgroundColor,
-                    pointRadius: 0,
-                    tension: 0.2,
-                })),
-            },
-            options: {
-                interaction: {
-                    intersect: false,
-                    mode: 'index',
-                    position: 'cursor',
-                },
-                scales: {
-                    x: {
-                        //type: 'time',
-                        grid: {
-                            //display : false,
-                        },
-                    },
-                    y: {
-                        suggestedMin: 0,
-                        suggestedMax: 10,
-                        ticks: {
-                            // Include a dollar sign in the ticks
-                            callback: function(value, index, ticks) {
-                                //console.log(value, index, ticks)
-                                return '$' + value;
-                            }
-                        },
-                    },
-                },
-                animation: {
-                    duration: 0
-                },
-                plugins: {
-                    legend: {
-                        display: false,
-                    },
-                    tooltip: {
-                        animation: false,
-                    },
-                },
-            },
-            plugins: [plugin],
-        };
-        new Chart(canvasId, config);
+        });
     }
 
 }
