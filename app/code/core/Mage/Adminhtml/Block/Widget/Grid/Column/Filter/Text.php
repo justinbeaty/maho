571e45acfc5807dbdf2decbebd34ab8709fe1dfb
Justin Beaty <justin.beaty@gmail.com>
2025-02-15 16:13:56 -0800

    Widget: Grid

--- app/code/core/Mage/Adminhtml/Block/Widget/Grid/Column/Filter/Text.php
+++ app/code/core/Mage/Adminhtml/Block/Widget/Grid/Column/Filter/Text.php
@@ -20,6 +20,6 @@ class Mage_Adminhtml_Block_Widget_Grid_Column_Filter_Text extends Mage_Adminhtml
     #[\Override]
     public function getHtml()
     {
-        return '<div class="field-100"><input type="text" name="' . $this->_getHtmlName() . '" id="' . $this->_getHtmlId() . '" value="' . $this->getEscapedValue() . '" class="input-text no-changes"/></div>';
+        return '<div><input type="text" name="' . $this->_getHtmlName() . '" id="' . $this->_getHtmlId() . '" value="' . $this->getEscapedValue() . '" class="input-text no-changes"/></div>';
     }
 }
